50f7058087211973f2fd80be3b6abfa41045c5ec
lukelyall <lukelyall2012@gmail.com>
2024-02-04 14:23:56 -0500

    receiving + displaying user information

--- githubrequest.js
+++ githubrequest.js
@@ -8,6 +8,29 @@ fetch(`https://api.github.com/users/${username}`)
     return response.json()
   })
   .then(data => {
-    console.log(data);
+    displayUserInfo(data);
   })
   .catch(error => console.error(error))
+
+  function displayUserInfo(user) {
+    // displaying information
+    const avatarElement = document.getElementById('avatar');
+    const usernameElement = document.getElementById('username');
+    const accountElement = document.getElementById('account');
+    const followersElement = document.getElementById('followers');
+    const followingElement = document.getElementById('following');
+    const reposElement = document.getElementById('public-repos');
+    
+    avatarElement.src = user.avatar_url;
+    usernameElement.textContent = user.login;
+    followersElement.textContent = `Followers: ${user.followers}`;
+    followingElement.textContent = `Following: ${user.following}`;
+    reposElement.textContent = `Public Repos: ${user.public_repos}`;
+
+    // creating an href for link to Github
+    const account = document.createElement('a');
+    account.href = user.html_url;
+    account.textContent = 'Github';
+    account.target = '_blank';
+    accountElement.appendChild(account);
+  }
